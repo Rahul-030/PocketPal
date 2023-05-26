52543d1e7933ed45fd1d720d14505c29e677b84a
Rahul semwal <semwalrahul03@gmail.com>
2023-05-26 22:42:27 +0530

    Add project files.

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PocketPal.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions Options) : base(Options)
+        {}
+
+
+        public DbSet<Transaction>Transactions { get; set; }
+        public DbSet<Category>Categories { get; set; }
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PocketPal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
